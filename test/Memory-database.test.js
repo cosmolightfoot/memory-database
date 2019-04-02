f492cfe68097323d4862a8a8147e8d8bda6ee0cd
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-02 16:51:15 -0700

    class function passes tests!

--- test/Memory-database.test.js
+++ test/Memory-database.test.js
@@ -1,9 +1,30 @@
 const MemoryDatabase = require('../lib/Memory-database');
 
 describe('memory database', () => {
+  let database1 = null;
+  let objectToSave = null;
+
+  beforeEach(() => {
+    database1 = new MemoryDatabase();
+    objectToSave = {
+      name: 'jonies',
+      quantity: 1029 
+    };
+  });
+
   it('has store', () => {
-    const database1 = new MemoryDatabase();
     expect(database1.store).toEqual({});
+  });
+
+  it('has an id', () => {
+    //call method "create" and pass an object as a value to a key named _id
+    const id = database1.create(objectToSave);
+    expect(database1.store[id]).toEqual(
+      { _id: id,
+        name: 'jonies',
+        quantity: 1029
+      }
+    );
   });
 });
 
