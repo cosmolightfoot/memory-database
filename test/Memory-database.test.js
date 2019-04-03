c15b55224fc5b982a97926de94696e40ca4708bd
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-02 17:19:55 -0700

    wrote find method with passing test

--- test/Memory-database.test.js
+++ test/Memory-database.test.js
@@ -3,6 +3,7 @@ const MemoryDatabase = require('../lib/Memory-database');
 describe('memory database', () => {
   let database1 = null;
   let objectToSave = null;
+  let objectToSave2 = null;
 
   beforeEach(() => {
     database1 = new MemoryDatabase();
@@ -10,6 +11,14 @@ describe('memory database', () => {
       name: 'jonies',
       quantity: 1029 
     };
+    objectToSave2 = {
+      name: 'guses',
+      quantity: 1 
+    };
+    // objectToSave3 = {
+    //   name: 'fishes',
+    //   quantity: 23 
+    // };
   });
 
   it('has store', () => {
@@ -25,6 +34,26 @@ describe('memory database', () => {
         quantity: 1029
       }
     );
+  });
+
+  it('has a find by id method', () => {
+    const id = database1.create(objectToSave);
+    expect(database1.findById(id)).toEqual(
+      { _id: id,
+        name: 'jonies',
+        quantity: 1029
+      }
+    );
+  });
+
+  it('returns list of all objects', () => {
+    const id = database1.create(objectToSave);
+    const id2 = database1.create(objectToSave2);
+      
+    expect(database1.find()).toEqual([ 
+      { _id: id, ...objectToSave }, 
+      { _id: id2, ...objectToSave2 }
+    ]);
   });
 });
 
